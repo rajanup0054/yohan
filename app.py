1249c77e406886a3b76537512b3d6ef3d85b2d8b
rajanup0054 <rajanup0054@gmail.com>
2024-11-11 19:39:35 +0530

    Initial commit

--- app.py
+++ app.py
@@ -44,4 +44,5 @@ def predict():
         return f"An error occurred: {e}"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host="0.0.0.0", port=port)
